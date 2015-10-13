5b70927be766b1129983fa55726c8fae81ef5e96
Ryan Arana <ryan@ryanarana.com>
2015-10-13 07:48:56 -0700

    Add some very basic animations

--- RoShamBo/RoShamBo/ViewController.swift
+++ RoShamBo/RoShamBo/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var winnerImage: UIImageView!
     @IBOutlet weak var actionLabel: UILabel!
     @IBOutlet weak var loserImage: UIImageView!
+    @IBOutlet weak var loserOverlay: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,6 +56,32 @@ class ViewController: UIViewController {
         }
 
         actionLabel.text = result.summary
+
+        UIView.animateWithDuration(0.3, animations: { self.winnerImage.center.y += 100 }) { _ in
+            UIView.animateWithDuration(0.3) { self.winnerImage.center.y -= 100 }
+
+            switch Verb(result) {
+            case .crushes:
+                // TODO: Use transforms instead, this doesn't look quite the way I'd like it to yet.
+                UIView.animateWithDuration(0.6, delay: 0, options: [.CurveEaseIn], animations: {
+                    self.loserImage.bounds.size.height = 10
+                    self.loserImage.center.y += self.winnerImage.bounds.height / 2.0
+                    }, completion: nil)
+                UIView.animateWithDuration(0.3, delay: 1.5, options: [], animations: {
+                    self.loserImage.bounds.size.height = self.winnerImage.bounds.size.height
+                    self.loserImage.center.y -= self.winnerImage.bounds.height / 2.0
+                    self.loserImage.contentMode = .ScaleAspectFit
+                    }, completion: nil)
+            default:
+                UIView.animateWithDuration(0.15, animations: { self.loserImage.center.x += 50 }) { _ in
+                    UIView.animateWithDuration(0.15, animations: { self.loserImage.center.x -= 100 }) { _ in
+                        UIView.animateWithDuration(0.15, animations: { self.loserImage.center.x += 100 }) { _ in
+                            UIView.animateWithDuration(0.15) { self.loserImage.center.x -= 50 }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
